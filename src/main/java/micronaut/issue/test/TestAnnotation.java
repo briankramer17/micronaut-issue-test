3deba1139b27dc0aef3614397511cc8085aed1e9
Brian Kramer <brian.kramer@tazworks.com>
2020-07-07 16:58:49 -0600

    - Compile errors with @Requires

--- src/main/java/micronaut/issue/test/TestAnnotation.java
+++
@@ -1,8 +0,0 @@
-package micronaut.issue.test;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target({ElementType.PACKAGE, ElementType.TYPE, ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.METHOD})
-@TestAnnotation
-public @interface TestAnnotation { }
